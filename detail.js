45e03a252ff5fe61c4adae9fd6f5be5092f01cb4
覃运 <2839568751@qq.com>
2023-03-09 22:48:35 +0800

    commit

---
+++ detail.js
@@ -0,0 +1,103 @@
+window.onload = function () {
+
+		// 获取页面元素
+	const productDetails = document.getElementById('detail');
+	const categoryH1 = document.getElementById('category');
+
+	// 从 URL 参数中获取商品 ID 和分类
+	const searchParams = new URLSearchParams(location.search);
+	const id = searchParams.get('id');
+	const category = searchParams.get('category');
+
+	// 获取商品信息
+	const product = products[id]
+
+	// 更新页面标题和分类
+	document.title = `${product.name} - ${category}`;
+	categoryH1.textContent = category;
+
+	// 创建商品详情元素
+	const productDiv = createProductElement(product);
+
+	// 添加商品详情元素到页面
+	productDetails.appendChild(productDiv);
+
+}
+
+// 获取商品信息
+function getProductById(id) {
+  const product = products.find(product => product.id === id);
+  if (!product) {
+    throw new Error(`Product with ID "${id}" not found.`);
+  }
+  return product;
+}
+
+// 创建商品详情元素
+function createProductElement(product) {
+  const productDiv = document.createElement('div');
+  productDiv.className = 'product';
+
+  const detail = document.createElement('h2');
+  detail.textContent = '商品详情';
+  productDiv.appendChild(detail);
+
+  const name = document.createElement('p');
+  name.textContent = `名称：${product.name}`;
+  productDiv.appendChild(name);
+
+  const material = document.createElement('p');
+  material.textContent = `材质：${product.material}`;
+  productDiv.appendChild(material);
+
+  const size = document.createElement('p');
+  size.textContent = `尺寸：${product.size}`;
+  productDiv.appendChild(size);
+
+  const price = document.createElement('p');
+  price.textContent = `价格：¥${product.price}`;
+  productDiv.appendChild(price);
+
+  const description = document.createElement('p');
+  description.textContent = product.description;
+  productDiv.appendChild(description);
+
+  const picturesDiv = document.createElement('div');
+  picturesDiv.className = 'product-pictures';
+  productDiv.appendChild(picturesDiv);
+
+  product.pictures.forEach(pictureUrl => {
+    const img = document.createElement('img');
+    img.src = pictureUrl;
+    picturesDiv.appendChild(img);
+    img.addEventListener('click', function() {
+    // create a new image element to display the enlarged image
+    const enlargedImg = document.createElement('img');
+    enlargedImg.src = pictureUrl;
+    enlargedImg.style.maxWidth = '90%'; // set the maximum width of the image
+
+    // create a new div to display the enlarged image
+    const enlargedImgDiv = document.createElement('div');
+    enlargedImgDiv.style.display = 'flex';
+    enlargedImgDiv.style.justifyContent = 'center';
+    enlargedImgDiv.style.alignItems = 'center';
+    enlargedImgDiv.style.position = 'fixed';
+    enlargedImgDiv.style.top = '0';
+    enlargedImgDiv.style.left = '0';
+    enlargedImgDiv.style.width = '100%';
+    enlargedImgDiv.style.height = '100%';
+    enlargedImgDiv.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+    enlargedImgDiv.appendChild(enlargedImg);
+
+    // add the enlarged image div to the document
+    document.body.appendChild(enlargedImgDiv);
+
+    // add an event listener to the enlarged image div to close it when clicked
+    enlargedImgDiv.addEventListener('click', function() {
+      document.body.removeChild(enlargedImgDiv);
+    });
+  });
+  });
+
+  return productDiv;
+}
---
+++ index.js
@@ -0,0 +1,145 @@
+// 创建一个新的XMLHttpRequest对象
+// var xhr = new XMLHttpRequest();
+
+// // 设置请求的类型、URL以及是否异步处理请求
+// xhr.open('GET', 'products.json', true);
+
+// // 定义当请求完成后执行的回调函数
+// xhr.onload = function() {
+// 	// 请求成功完成
+// 	if (xhr.status === 200) {
+// 		// 将响应文本解析为JSON格式
+// 		var products = JSON.parse(xhr.responseText);
+
+// 		// 将每个紫砂壶商品显示在页面上
+// 		var productList = document.getElementById('product-list');
+// 		for (var i = 0; i < products.length; i++) {
+// 			var product = products[i];
+
+// 			// 创建一个新的<div>元素，用于显示该紫砂壶商品的信息
+// 			var productDiv = document.createElement('div');
+// 			productDiv.innerHTML = '<h2>' + product.name + '</h2><p>' + product.description + '</p><p>价格：' + product.price + '元</p>';
+
+// 			// 将<div>元素添加到页面中
+// 			productList.appendChild(productDiv);
+// 		}
+// 	} else {
+// 		// 请求失败，输出错误消息
+// 		console.log('请求失败：' + xhr.status);
+// 	}
+// };
+
+// // 发送HTTP请求
+// xhr.send();
+
+
+
+// window.onload = function (){
+
+//     fetch('product.json')
+//         .then(response => response.json())
+//         .then(data => {
+//           const parsedData = JSON.parse(data);
+//           console.log(parsedData);
+//         })
+//         .catch(error => console.error(error));
+
+// }
+
+
+// let products = [
+
+// 	{
+// 		img: "https://picsum.photos/id/1000/400/300",
+// 		pictures: [
+// 			"https://picsum.photos/id/1000/400/300",
+// 			"https://picsum.photos/id/1000/400/300",
+// 			"https://picsum.photos/id/1000/400/300"
+// 		],
+// 		name: "阳泉紫砂壶",
+// 		material: "紫砂泥",
+// 		size: "12cm x 12cm x 8cm",
+// 		price: "￥99.99",
+// 		description: "这是一件精美的阳泉紫砂壶，手工制作，外观精致，内部光滑。"
+// 	},
+// 	{
+// 		img: "https://picsum.photos/id/1000/400/300",
+// 		pictures: [
+// 			"https://picsum.photos/id/1000/400/300",
+// 			"https://picsum.photos/id/1000/400/300",
+// 			"https://picsum.photos/id/1000/400/300"
+// 		],
+// 		name: "阳泉紫砂壶",
+// 		material: "紫砂泥",
+// 		size: "12cm x 12cm x 8cm",
+// 		price: "￥99.99",
+// 		description: "这是一件精美的阳泉紫砂壶，手工制作，外观精致，内部光滑。"
+// 	},
+// 	{
+// 		img: "https://picsum.photos/id/1000/400/300",
+// 		pictures: [
+// 			"https://picsum.photos/id/1000/400/300",
+// 			"https://picsum.photos/id/1000/400/300",
+// 			"https://picsum.photos/id/1000/400/300"
+// 		],
+// 		name: "阳泉紫砂壶",
+// 		material: "紫砂泥",
+// 		size: "12cm x 12cm x 8cm",
+// 		price: "￥99.99",
+// 		description: "这是一件精美的阳泉紫砂壶，手工制作，外观精致，内部光滑。"
+// 	},
+// ]
+
+
+
+window.onload = function () {
+
+	// 将每个紫砂壶商品显示在页面上
+	var productList = document.getElementById('product-list');
+
+	for (var i = 0; i < products.length; i++) 
+	{
+		var product = products[i];
+
+		// 创建一个新的<div>元素，用于显示该紫砂壶商品的信息
+		var productDiv = document.createElement('div');
+
+		var img = document.createElement("img")
+		var name = document.createElement("h3")
+		var material = document.createElement("p")
+		var size = document.createElement("p")
+		var price = document.createElement("p")
+		var button = document.createElement("button")
+
+		img.src = product.img
+
+		name.innerHTML = product.name
+		material.innerHTML = product.material
+		size.innerHTML = product.size
+		price.innerHTML = product.price
+		button.innerHTML = "查看详情"
+		button.tag = i
+		button.addEventListener('click', function() {
+			window.location.href = "detail.html?id=" + this.tag + "&categroy=传统紫砂壶"
+		})
+
+		
+		productDiv.appendChild(img)
+		productDiv.appendChild(name)
+		productDiv.appendChild(material)
+		productDiv.appendChild(size)
+		productDiv.appendChild(price)
+		productDiv.appendChild(button)
+		productDiv.className = "product"
+	
+		productList.appendChild(productDiv)
+
+
+		// productDiv.innerHTML = '<h2>' + product.name + '</h2><p>' + product.description + '</p><p>价格：' + product.price + '元</p>';
+
+		// // 将<div>元素添加到页面中
+		// productList.appendChild(productDiv);
+	}
+}
+
+
