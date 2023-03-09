e2c732d53b9d81b548da73519b43b58e605afa3f
覃运 <2839568751@qq.com>
2023-03-09 23:07:44 +0800

    update

--- js/data.js
+++ js/data.js
@@ -1,16 +1,14 @@
 let products = [
 
 	{
-		img: "https://picsum.photos/id/1000/400/300",
+		img: "res/202303090001.jpg",
 		pictures: [
-			"https://picsum.photos/id/1000/400/300",
-			"https://picsum.photos/id/1000/400/300",
-			"https://picsum.photos/id/1000/400/300",
-			"https://picsum.photos/id/1000/400/300",
-			"https://picsum.photos/id/1000/400/300",
-			"https://picsum.photos/id/1000/400/300",
-			"https://picsum.photos/id/1000/400/300",
-			"https://picsum.photos/id/1000/400/300"
+			"res/202303090002.jpg",
+			"res/202303090003.jpg",
+			"res/202303090004.jpg",
+			"res/202303090005.jpg",
+			"res/202303090006.jpg",
+			"res/202303090007.jpg"
 		],
 		name: "阳泉紫砂壶",
 		material: "紫砂泥",
@@ -19,11 +17,14 @@ let products = [
 		description: "这是一件精美的阳泉紫砂壶，手工制作，外观精致，内部光滑。"
 	},
 	{
-		img: "https://picsum.photos/id/1000/400/300",
+		img: "res/202303090001.jpg",
 		pictures: [
-			"https://picsum.photos/id/1000/400/300",
-			"https://picsum.photos/id/1000/400/300",
-			"https://picsum.photos/id/1000/400/300"
+			"res/202303090002.jpg",
+			"res/202303090003.jpg",
+			"res/202303090004.jpg",
+			"res/202303090005.jpg",
+			"res/202303090006.jpg",
+			"res/202303090007.jpg"
 		],
 		name: "阳泉紫砂壶",
 		material: "紫砂泥",
@@ -32,11 +33,62 @@ let products = [
 		description: "这是一件精美的阳泉紫砂壶，手工制作，外观精致，内部光滑。"
 	},
 	{
-		img: "https://picsum.photos/id/1000/400/300",
+		img: "res/202303090001.jpg",
 		pictures: [
-			"https://picsum.photos/id/1000/400/300",
-			"https://picsum.photos/id/1000/400/300",
-			"https://picsum.photos/id/1000/400/300"
+			"res/202303090002.jpg",
+			"res/202303090003.jpg",
+			"res/202303090004.jpg",
+			"res/202303090005.jpg",
+			"res/202303090006.jpg",
+			"res/202303090007.jpg"
+		],
+		name: "阳泉紫砂壶",
+		material: "紫砂泥",
+		size: "12cm x 12cm x 8cm",
+		price: "￥99.99",
+		description: "这是一件精美的阳泉紫砂壶，手工制作，外观精致，内部光滑。"
+	},
+	{
+		img: "res/202303090001.jpg",
+		pictures: [
+			"res/202303090002.jpg",
+			"res/202303090003.jpg",
+			"res/202303090004.jpg",
+			"res/202303090005.jpg",
+			"res/202303090006.jpg",
+			"res/202303090007.jpg"
+		],
+		name: "阳泉紫砂壶",
+		material: "紫砂泥",
+		size: "12cm x 12cm x 8cm",
+		price: "￥99.99",
+		description: "这是一件精美的阳泉紫砂壶，手工制作，外观精致，内部光滑。"
+	},
+	{
+		img: "res/202303090001.jpg",
+		pictures: [
+			"res/202303090002.jpg",
+			"res/202303090003.jpg",
+			"res/202303090004.jpg",
+			"res/202303090005.jpg",
+			"res/202303090006.jpg",
+			"res/202303090007.jpg"
+		],
+		name: "阳泉紫砂壶",
+		material: "紫砂泥",
+		size: "12cm x 12cm x 8cm",
+		price: "￥99.99",
+		description: "这是一件精美的阳泉紫砂壶，手工制作，外观精致，内部光滑。"
+	},
+	{
+		img: "res/202303090001.jpg",
+		pictures: [
+			"res/202303090002.jpg",
+			"res/202303090003.jpg",
+			"res/202303090004.jpg",
+			"res/202303090005.jpg",
+			"res/202303090006.jpg",
+			"res/202303090007.jpg"
 		],
 		name: "阳泉紫砂壶",
 		material: "紫砂泥",
