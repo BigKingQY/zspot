b2254aa7f0dcdeb93890c329065cfb3f36c6aac0
覃运 <2839568751@qq.com>
2023-03-11 15:57:00 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,101 +1,22 @@
-// 创建一个新的XMLHttpRequest对象
-// var xhr = new XMLHttpRequest();
-
-// // 设置请求的类型、URL以及是否异步处理请求
-// xhr.open('GET', 'products.json', true);
-
-// // 定义当请求完成后执行的回调函数
-// xhr.onload = function() {
-// 	// 请求成功完成
-// 	if (xhr.status === 200) {
-// 		// 将响应文本解析为JSON格式
-// 		var products = JSON.parse(xhr.responseText);
-
-// 		// 将每个紫砂壶商品显示在页面上
-// 		var productList = document.getElementById('product-list');
-// 		for (var i = 0; i < products.length; i++) {
-// 			var product = products[i];
-
-// 			// 创建一个新的<div>元素，用于显示该紫砂壶商品的信息
-// 			var productDiv = document.createElement('div');
-// 			productDiv.innerHTML = '<h2>' + product.name + '</h2><p>' + product.description + '</p><p>价格：' + product.price + '元</p>';
-
-// 			// 将<div>元素添加到页面中
-// 			productList.appendChild(productDiv);
-// 		}
-// 	} else {
-// 		// 请求失败，输出错误消息
-// 		console.log('请求失败：' + xhr.status);
-// 	}
-// };
-
-// // 发送HTTP请求
-// xhr.send();
-
-
-
-// window.onload = function (){
-
-//     fetch('product.json')
-//         .then(response => response.json())
-//         .then(data => {
-//           const parsedData = JSON.parse(data);
-//           console.log(parsedData);
-//         })
-//         .catch(error => console.error(error));
-
-// }
-
-
-// let products = [
-
-// 	{
-// 		img: "https://picsum.photos/id/1000/400/300",
-// 		pictures: [
-// 			"https://picsum.photos/id/1000/400/300",
-// 			"https://picsum.photos/id/1000/400/300",
-// 			"https://picsum.photos/id/1000/400/300"
-// 		],
-// 		name: "阳泉紫砂壶",
-// 		material: "紫砂泥",
-// 		size: "12cm x 12cm x 8cm",
-// 		price: "￥99.99",
-// 		description: "这是一件精美的阳泉紫砂壶，手工制作，外观精致，内部光滑。"
-// 	},
-// 	{
-// 		img: "https://picsum.photos/id/1000/400/300",
-// 		pictures: [
-// 			"https://picsum.photos/id/1000/400/300",
-// 			"https://picsum.photos/id/1000/400/300",
-// 			"https://picsum.photos/id/1000/400/300"
-// 		],
-// 		name: "阳泉紫砂壶",
-// 		material: "紫砂泥",
-// 		size: "12cm x 12cm x 8cm",
-// 		price: "￥99.99",
-// 		description: "这是一件精美的阳泉紫砂壶，手工制作，外观精致，内部光滑。"
-// 	},
-// 	{
-// 		img: "https://picsum.photos/id/1000/400/300",
-// 		pictures: [
-// 			"https://picsum.photos/id/1000/400/300",
-// 			"https://picsum.photos/id/1000/400/300",
-// 			"https://picsum.photos/id/1000/400/300"
-// 		],
-// 		name: "阳泉紫砂壶",
-// 		material: "紫砂泥",
-// 		size: "12cm x 12cm x 8cm",
-// 		price: "￥99.99",
-// 		description: "这是一件精美的阳泉紫砂壶，手工制作，外观精致，内部光滑。"
-// 	},
-// ]
-
-
 
 window.onload = function () {
 
 	// 将每个紫砂壶商品显示在页面上
 	var productList = document.getElementById('product-list');
+
+		// 检测用户代理字符串
+	const userAgent = navigator.userAgent;
+	const mobileKeywords = ['Mobile', 'iPhone', 'Android', 'Windows Phone', 'BlackBerry'];
+	const isMobile = mobileKeywords.some(keyword => userAgent.includes(keyword));
+
+	// 根据结果执行不同的代码
+	if (isMobile) {
+	  productList.className = "product-list-mobile"
+	} else {
+	  productList.className = "product-list-pc"
+	}
+
+	console.log(isMobile)
 
 	for (var i = 0; i < products.length; i++) 
 	{
@@ -112,6 +33,13 @@ window.onload = function () {
 		var button = document.createElement("button")
 
 		img.src = product.img
+		if (isMobile)
+		{
+			img.style.height = "100px"
+		} else {
+			img.style.height = "200px"
+		}
+
 		name.innerHTML = product.name
 		material.innerHTML = product.material
 		size.innerHTML = product.size
